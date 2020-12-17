5673babff65d835701ec4f10d341f1956ea0b343
Pruthvi <psambu@martinonesource.com>
2020-12-17 14:43:17 -0600

    implementing actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import App from './Components/App';
+import reducers from './reducers/reducer';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ src/reducers/reducer.js
@@ -0,0 +1,35 @@
+import { combineReducers } from 'redux';
+const songReducer = () => {
+    return [
+        {
+            title: 'Levitating',
+            duration: '4.50'
+        },
+        {
+            title: 'Uptown Funk',
+            duration: '3.20'
+        },
+        {
+            title: 'The Blinding Lights',
+            duration: '5.20'
+        },
+        {
+            title: 'Criminal',
+            duration: '5.00'
+        }
+    ];
+}
+
+const songSelectedReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+}
+
+export default combineReducers = () => {
+    return {
+        songs: songReducer,
+        selectedSong: songSelectedReducer
+    }
+}
